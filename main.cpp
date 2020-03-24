b6ac08d5f95154aa833979f1087c3c8f1547f637
Waldemar <41773009+integral96@users.noreply.github.com>
2020-03-24 21:02:07 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,42 @@
 #include <matrix_initialised.hpp>
 
+
+#include <boost/lexical_cast.hpp>
+
+boost::mutex mutex;
+
+void time_multiple(boost::promise<std::string>& pr, multyplyMatrix& m_mtrx) {
+    std::string id = boost::lexical_cast<std::string>(boost::this_thread::get_id());
+    m_mtrx.init_multiply_Meta();
+    std::cout << "Print thread " << id << " starting.\n";
+    boost::this_thread::sleep_for(boost::chrono::seconds(15));
+    pr.set_value(id);
+}
+void thrFunc(boost::shared_future<std::string> sf, int waitfor) {
+    while (sf.wait_for(boost::chrono::seconds(waitfor)) == boost::future_status::timeout) {
+        boost::lock_guard<boost::mutex> lg(mutex);
+        std::cout << "Subscriber thread " << boost::this_thread::get_id() << " waiting...\n";
+    }
+    std::cout << "\nSubscriber thread " << boost::this_thread::get_id() << " got " << sf.get() << std::endl;
+}
 int main() {
-    init_multiply_matrix();
+    multyplyMatrix mtrx;
+    boost::promise<std::string> prom;
+    boost::future<std::string> futur(prom.get_future());
+    boost::shared_future<std::string> shfut(std::move(futur));
+
+    boost::thread print_mtrx(boost::bind(time_multiple, boost::ref(prom), boost::ref(mtrx)));
+
+    boost::thread subscribe1(boost::bind(thrFunc, shfut, 2));
+    boost::thread subscribe2(boost::bind(thrFunc, shfut, 4));
+    boost::thread subscribe3(boost::bind(thrFunc, shfut, 6));
+
+    print_mtrx.join();
+    subscribe1.join();
+    subscribe2.join();
+    subscribe3.join();
+
 
 	return 0;
 }
+
